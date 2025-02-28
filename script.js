aedcb746e67d922ee32aced7bcc6c9edde672f16
Emman Sadiang-abay <84630568+EngrKulot@users.noreply.github.com>
2025-02-28 23:03:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    const textArray = [
+        "I design & code stunning web experiences.",
+        "Creating clean, modern, and responsive websites.",
+        "Bringing ideas to life with code."
+    ];
+    let index = 0;
+    let charIndex = 0;
+    let typingElement = document.getElementById("typing");
+
+    function typeEffect() {
+        if (charIndex < textArray[index].length) {
+            typingElement.innerHTML += textArray[index].charAt(charIndex);
+            charIndex++;
+            setTimeout(typeEffect, 100);
+        } else {
+            setTimeout(eraseEffect, 2000);
+        }
+    }
+
+    function eraseEffect() {
+        if (charIndex > 0) {
+            typingElement.innerHTML = textArray[index].substring(0, charIndex - 1);
+            charIndex--;
+            setTimeout(eraseEffect, 50);
+        } else {
+            index = (index + 1) % textArray.length;
+            setTimeout(typeEffect, 500);
+        }
+    }
+
+    typeEffect();
+});
