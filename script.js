c2e17d0c270dbc6c5d1f920c79d0b3b416e21f55
Emman Sadiang-abay <84630568+EngrKulot@users.noreply.github.com>
2025-03-02 16:32:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -36,9 +36,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const menuToggle = document.querySelector(".menu-toggle");
     const navMenu = document.querySelector("nav");
 
-    if (menuToggle && navMenu) {
-        menuToggle.addEventListener("click", function () {
-            navMenu.classList.toggle("active");
+    
+    if (menuToggle && nav) {
+        menuToggle.addEventListener("click", () => {
+            nav.classList.toggle("active");
         });
     }
 });
