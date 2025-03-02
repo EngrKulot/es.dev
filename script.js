42b7dbd69ce37d40dd0ddde174429a73360d3090
Emman Sadiang-abay <84630568+EngrKulot@users.noreply.github.com>
2025-03-02 17:13:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -34,19 +34,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Mobile Menu Toggle
     const menuToggle = document.querySelector(".menu-toggle");
-    const navMenu = document.querySelector("nav");
+    const nav = document.querySelector("nav");
 
-    
     if (menuToggle && nav) {
         menuToggle.addEventListener("click", () => {
             nav.classList.toggle("active");
         });
-    }
-    // Close menu when clicking outside
-    document.addEventListener("click", (event) => {
-        if (!nav.contains(event.target) && !menuToggle.contains(event.target)) {
-            nav.classList.remove("active");
-        }
-    });
 
+        // Close menu when clicking outside
+        document.addEventListener("click", (event) => {
+            if (!nav.contains(event.target) && !menuToggle.contains(event.target)) {
+                nav.classList.remove("active");
+            }
+        });
+    }
 });
